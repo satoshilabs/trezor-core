aae13efd836d37d4814dc00eddfbc0987b4ff474
Adrian Matejov <a.matejov@centrum.sk>
2018-07-11 13:58:58 +0200

    tezos: started rewriting sign_tx
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- src/apps/tezos/layout.py
+++ src/apps/tezos/layout.py
@@ -1,25 +1,62 @@
-from apps.common.confirm import *
 from trezor import ui
-from trezor.utils import chunks
 from trezor.messages import ButtonRequestType
 from trezor.ui.text import Text
+from trezor.utils import chunks
+
+from apps.common.confirm import *
 
 
 async def require_confirm_tx(ctx, to, value):
-    content = Text('Confirm sending', ui.ICON_SEND,
-                   ui.BOLD, format_amount(value),
-                   ui.NORMAL, 'to',
-                   ui.MONO, *split_address(to),
-                   icon_color=ui.GREEN)
+    content = Text(
+        "Confirm sending",
+        ui.ICON_SEND,
+        ui.BOLD,
+        format_amount(value),
+        ui.NORMAL,
+        "to",
+        ui.MONO,
+        *split_address(to),
+        icon_color=ui.GREEN
+    )
     return await require_confirm(ctx, content, ButtonRequestType.SignTx)
 
 
 async def require_confirm_fee(ctx, value, fee):
-    content = Text('Confirm transaction', ui.ICON_SEND,
-                   ui.BOLD, format_amount(value),
-                   ui.NORMAL, 'fee:',
-                   ui.BOLD, format_amount(fee),
-                   icon_color=ui.GREEN)
+    content = Text(
+        "Confirm transaction",
+        ui.ICON_SEND,
+        ui.BOLD,
+        format_amount(value),
+        ui.NORMAL,
+        "fee:",
+        ui.BOLD,
+        format_amount(fee),
+        icon_color=ui.GREEN,
+    )
+    await require_hold_to_confirm(ctx, content, ButtonRequestType.SignTx)
+
+
+# TODO
+async def require_confirm_delegation(ctx):
+    content = Text(
+        "Confirm origination", ui.ICON_SEND, ui.BOLD, "TODO", icon_color=ui.GREEN
+    )
+    await require_hold_to_confirm(ctx, content, ButtonRequestType.SignTx)
+
+
+# TODO: check if it we want to show more info
+async def require_confirm_delegation(ctx, to, fee):
+    content = Text(
+        "Confirm delegation",
+        ui.ICON_SEND,
+        ui.BOLD,
+        "fee: " + format_amount(fee),
+        ui.NORMAL,
+        "to",
+        ui.MONO,
+        *split_address(to),
+        icon_color=ui.GREEN
+    )
     await require_hold_to_confirm(ctx, content, ButtonRequestType.SignTx)
 
 
@@ -28,5 +65,5 @@ def split_address(address):
 
 
 def format_amount(value):
-    # TODO: check if value has to be divided by some number
-    return '%s XTZ' % value
+    # TODO: divide value
+    return "%s XTZ" % value
--- src/apps/tezos/sign_tx.py
+++ src/apps/tezos/sign_tx.py
@@ -1,27 +1,116 @@
-from apps.common import seed
-from trezor.messages.TezosSignedTx import TezosSignedTx
+import ustruct
+
+from trezor import wire
 from trezor.crypto import hashlib
 from trezor.crypto.curve import ed25519
+from trezor.messages import TezosContractType
+from trezor.messages.TezosSignedTx import TezosSignedTx
 
-from apps.tezos.helpers import b58cencode, TEZOS_CURVE
+from apps.common import seed
+from apps.tezos.helpers import TEZOS_CURVES, b58cencode
 from apps.tezos.layout import *
 
 
 async def tezos_sign_tx(ctx, msg):
     address_n = msg.address_n or ()
-    node = await seed.derive_node(ctx, address_n, TEZOS_CURVE)
+    node = await seed.derive_node(ctx, address_n, "ed25519")
 
-    operation_bytes = msg.operation_bytes
-    operation_hashed_bytes = hashlib.blake2b(operation_bytes, 32).digest()
-    signature = ed25519.sign(node.private_key(), operation_hashed_bytes)
+    # TODO: check if signing differs according to public_key_hash in source
 
-    sig_op_contents = operation_bytes + signature
-    sig_op_contents_blaked = hashlib.blake2b(sig_op_contents, 32).digest()
-    operation_hash = b58cencode(sig_op_contents_blaked, prefix='o')
+    # operation_bytes = msg.operation_bytes
+    # operation_hashed_bytes = hashlib.blake2b(operation_bytes, outlen=32).digest()
+    # signature = ed25519.sign(node.private_key(), operation_hashed_bytes)
 
-    await require_confirm_tx(ctx, msg.to, msg.value)
-    await require_confirm_fee(ctx, msg.value, msg.fee)
+    # sig_op_contents = operation_bytes + signature
+    # sig_op_contents_blaked = hashlib.blake2b(sig_op_contents, outlen=32).digest()
+    # operation_hash = b58cencode(sig_op_contents_blaked, prefix='o')
 
-    return TezosSignedTx(signature=signature,
-                         sig_op_contents=sig_op_contents,
-                         operation_hash=operation_hash)
+    to = _get_address_by_type(msg.transaction.destination)
+
+    # print(list(_get_operation_bytes(msg)))
+
+    # TODO: raise invalid operation type if not supported
+
+    await require_confirm_delegation(ctx, to, msg.operation.fee)
+    # TODO: show layout according to OperationType
+    await require_confirm_tx(ctx, to, msg.transaction.amount)
+    await require_confirm_fee(ctx, msg.transaction.amount, msg.operation.fee)
+
+    return TezosSignedTx(
+        signature=bytes([1, 2, 3]),
+        sig_op_contents=bytes([1, 2, 3]),
+        operation_hash="testhash",
+    )
+
+
+def _get_address_by_type(address):
+    if address.tag == TezosContractType.Implicit:
+        # TODO: check if the prefix differs according to hash tag
+        return b58cencode(address.hash[1:], prefix="tz1")
+
+    elif address.tag == TezosContractType.Originated:
+        return b58cencode(address.hash[:-1], prefix="KT1")
+
+    raise wire.DataError("Invalid contract type")
+
+
+def _get_operation_bytes(msg):
+    result = msg.operation.branch
+
+    # common part
+    result += ustruct.pack("<b", msg.operation.tag)
+    result += _encode_contract_id(msg.operation.source)
+    result += _encode_zarith(msg.operation.fee)
+    result += _encode_zarith(msg.operation.counter)
+    result += _encode_zarith(msg.operation.gas_limit)
+    result += _encode_zarith(msg.operation.storage_limit)
+
+    if msg.transaction is not None:
+        # transaction part
+        result += _encode_zarith(msg.transaction.amount)
+        result += _encode_contract_id(msg.transaction.destination)
+        result += _encode_data_with_bool_prefix(msg.transaction.parameters)
+    elif msg.origination is not None:
+        # origination part
+        result += msg.origination.manager_pubkey
+        result += _encode_zarith(msg.origination.balance)
+        result += _encode_bool(msg.origination.spendable)
+        result += _encode_bool(msg.origination.delegatable)
+        result += _encode_data_with_bool_prefix(msg.origination.delegate)
+        result += _encode_data_with_bool_prefix(msg.origination.script)
+    elif msg.delegation is not None:
+        # delegation part
+        result += _encode_data_with_bool_prefix(msg.delegation.delegate)
+
+    else:
+        raise wire.DataError("Invalid operation type")
+
+    return result
+
+
+def _encode_contract_id(contract_id):
+    return ustruct.pack("<b", contract_id.tag) + contract_id.hash
+
+
+def _encode_bool(boolean):
+    return ustruct.pack("<b", 255) if boolean else ustruct.pack("<b", 0)
+
+
+def _encode_data_with_bool_prefix(data):
+    return _encode_bool(True) + data if data is not None else _encode_bool(False)
+
+
+def _encode_zarith(num):
+    result = bytes()
+
+    while True:
+        byte = num & 127
+        num = num >> 7
+
+        if num == 0:
+            result += ustruct.pack("<b", byte)
+            break
+
+        result += ustruct.pack("<b", 128 | byte)
+
+    return result
