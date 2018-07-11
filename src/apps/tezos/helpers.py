697b4457f35bff4100e812db1701295196724c2b
Adrian Matejov <a.matejov@centrum.sk>
2018-07-11 13:56:16 +0200

    tezos: different curves in helpers
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- src/apps/tezos/helpers.py
+++ src/apps/tezos/helpers.py
@@ -1,16 +1,59 @@
+from trezor import wire
 from trezor.crypto import base58
-
+from trezor.crypto.curve import ed25519, nist256p1, secp256k1
 
 PREFIXES = {
-    'tz1': [6, 161, 159],
-    'edpk': [13, 15, 37, 217],
-    'edsk': [43, 246, 78, 7],
-    'edsig': [9, 245, 205, 134, 18],
-    'o': [5, 116],
-    'KT1': [2, 90, 121]
+    # addresses
+    "tz1": [6, 161, 159],
+    "tz2": [6, 161, 161],
+    "tz3": [6, 161, 164],
+    "KT1": [2, 90, 121],
+    # public keys
+    "edpk": [13, 15, 37, 217],
+    "sppk": [3, 254, 226, 86],
+    "p2pk": [3, 178, 139, 127],
+    # operation hash
+    "o": [5, 116],
 }
 
-TEZOS_CURVE = 'ed25519'
+TEZOS_CURVES = [
+    {
+        "name": "ed25519",
+        "address_prefix": "tz1",
+        "pk_prefix": "edpk",
+        "module": ed25519,
+    },
+    {
+        "name": "secp256k1",
+        "address_prefix": "tz2",
+        "pk_prefix": "sppk",
+        "module": secp256k1,
+    },
+    {
+        "name": "nist256p1",
+        "address_prefix": "tz3",
+        "pk_prefix": "p2pk",
+        "module": nist256p1,
+    },
+]
+
+
+def get_curve_name(index):
+    if 0 <= index < len(TEZOS_CURVES):
+        return TEZOS_CURVES[index]["name"]
+    raise wire.DataError("Invalid type of curve")
+
+
+def get_curve_module(curve):
+    return TEZOS_CURVES[curve]["module"]
+
+
+def get_address_prefix(curve):
+    return TEZOS_CURVES[curve]["address_prefix"]
+
+
+def get_pk_prefix(curve):
+    return TEZOS_CURVES[curve]["pk_prefix"]
 
 
 def b58cencode(payload, prefix=None):
@@ -24,5 +67,5 @@ def b58cencode(payload, prefix=None):
 def b58cdecode(enc, prefix=None):
     decoded = base58.decode_check(enc)
     if prefix is not None:
-        decoded = decoded[len(PREFIXES[prefix]):]
+        decoded = decoded[len(PREFIXES[prefix]) :]
     return decoded
