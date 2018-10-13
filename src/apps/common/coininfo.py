f99317be9935eec81f3258d9fd3f9dbe55918a79
Leandro Reinaux <leoreinaux@gmail.com>
2018-10-12 23:42:16 -0300

    fix code style and checks

--- src/apps/common/coininfo.py
+++ src/apps/common/coininfo.py
@@ -1,6 +1,6 @@
 # generated from coininfo.py.mako
 # do not edit manually!
-from trezor.crypto.base58 import blake256_32, groestl512d_32, sha256d_32, sha3k_256
+from trezor.crypto.base58 import blake256_32, groestl512d_32, sha3k_256, sha256d_32
 from trezor.crypto.scripts import blake256_ripemd160_digest, sha256_ripemd160_digest
 
 
