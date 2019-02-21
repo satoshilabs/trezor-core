0ff46041846a16113971ea14d2975638173a4d90
Jozef Fuker <barlog951@gmail.com>
2019-02-21 14:20:28 +0100

    change mnemonic

--- tests/test_apps.onegram.get_public_key.py
+++ tests/test_apps.onegram.get_public_key.py
@@ -7,7 +7,7 @@ from ubinascii import hexlify, unhexlify
 
 class TestEosGetPublicKey(unittest.TestCase):
     def test_get_public_key_scheme(self):
-        mnemonic = "one one one one one one one one one one one onegram"
+        mnemonic = "all all all all all all all all all all all all"
         seed = bip39.seed(mnemonic, '')
 
         derivation_paths = [
@@ -18,17 +18,17 @@ class TestEosGetPublicKey(unittest.TestCase):
         ]
 
         public_keys = [
-            b'0281c89a0ae5ec0b25426fac4739687202491ec67470cf45b50466794488dff356',
-            b'031d3459058196b93c1a4816f09de7798043af0e42b64379d5d5b9fa2bd1c926b3',
-            b'030307f8bd7a9bccb376087e3ecf332cce77ed27e82b2c6710b5c8494d205acc93',
-            b'027e57796bc3f2af650d8f17366380eb72b4fcb2ba810105906851878768bb9364',
+            b'034313262d3882c7de129afa9ff1639c9ff9881e2d54b444d1ad8f6e6291b3e13f',
+            b'0300bf07cdebdf37f25263697fab99070f5dc30bab13a830d243d37dab115a3427',
+            b'03af47021b410f1bc96f4cc98f776bf5360171b916aeb464c857bd68d6fceabd78',
+            b'02cd8354b2eb5b75f1c5d4f0f0da85f409701474b1aa93b7b5f1685d5be6b6b671',
         ]
 
         wif_keys = [
-            'OGC5seW3PZW6xkcYZWLT3ygRhet2eQq8JX9bXEeJGjQEaHjgv2U8y',
-            'OGC746X5TRVyJGPG6J5kHx4G13VMKQAdoDfqbyfkjFRVoL2k8EvxV',
-            'OGC6rZxRharERsdS11qNqwJZNMaFLmjLWAZNYJvLgiRsRHf3JQ8pi',
-            'OGC5r8akV2oRfmfwCvhZMLsnGPqyL6gDAK6W24gKe6cghBk18s4d9',
+            'OGC7LmsAtnUHdXsXbpZ7NnYNAsHS8LUn5wGbosiG7FEt52991efr6',
+            'OGC6qZbEZyRxQLkcZvZWwXh2ULirVWcxMtHro2mRpWShZCk5XLmcn',
+            'OGC8ARkmKjvefMcVmGCgDpb1ux72DaJPEjuQVXuvixgkurodNkvZ1',
+            'OGC6SzubhMkRakxS2TBX9Wrd13N9dSWf8VE1W7zS1WUii9DoLDwYr',
         ]
 
         for index, path in enumerate(derivation_paths):
