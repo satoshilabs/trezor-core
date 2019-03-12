c9d87fdc966e68937935af6c79973bd41225c1bf
Adrian Matejov <ado.matejov@gmail.com>
2019-03-12 16:44:53 +0100

    onegram: bip48 and simple unittests
    Signed-off-by: Adrian Matejov <ado.matejov@gmail.com>

--- src/apps/onegram/__init__.py
+++ src/apps/onegram/__init__.py
@@ -5,6 +5,6 @@ from apps.common import HARDENED
 
 
 def boot():
-    ns = [["secp256k1", HARDENED | 44, HARDENED | 274]]
+    ns = [["secp256k1", HARDENED | 48, HARDENED | 10]]
     wire.add(MessageType.OnegramGetPublicKey, __name__, "get_public_key", ns)
     wire.add(MessageType.OnegramSignTx, __name__, "sign_tx", ns)
---
+++ tests/test_apps.onegram.writers.py
@@ -0,0 +1,27 @@
+from ubinascii import hexlify
+
+from common import *
+from trezor.messages.OnegramAsset import OnegramAsset
+
+from apps.onegram import writers
+
+
+class TestOnegramWriters(unittest.TestCase):
+
+    def test_onegram_write_varint(self):
+        w = bytearray()
+        writers.write_varint(w, int(0))
+        self.assertEqual(bytes(w), bytes())
+
+        w = bytearray()
+        writers.write_varint(w, 1085)
+        self.assertEqual(hexlify(w), b"bd08")
+
+    def test_onegram_write_asset(self):
+        w = bytearray()
+        writers.write_asset(w, OnegramAsset(amount=10000, asset_id="1.3.0"))
+        self.assertEqual(hexlify(w), b"1027000000000000")
+
+
+if __name__ == "__main__":
+    unittest.main()
